434bea6ce79079b2a75f981d4cd90e24bc6d415e
Matteo Rotundo <rotundo.matteo@gmail.com>
2024-04-24 17:00:29 +0200

    Added CircuitBreaker for order service

--- order-service/src/main/java/org/gyro/orderservice/controller/OrderController.java
+++ order-service/src/main/java/org/gyro/orderservice/controller/OrderController.java
@@ -1,5 +1,6 @@
 package org.gyro.orderservice.controller;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.gyro.orderservice.dto.OrderRequest;
 import org.gyro.orderservice.service.OrderService;
@@ -15,8 +16,13 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
        orderService.placeOrder(orderRequest);
        return "Order Placed Succesfully";
+    }
+
+    public String fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
+        return "Oops! Something went wrong! Please order later!";
     }
 }
